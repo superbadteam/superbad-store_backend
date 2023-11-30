aa02a66896f39cab789893f20ac4066567c34723
Phạm Hồng Phúc <phamhongphuc1403@gmail.com>
2023-11-30 10:57:57 +0700

    enhance: add avatar url and cover url to users table (#17)

--- Services/Inventory/Core/InventoryManagement.Core.Domain/UserAggregate/Entities/User.cs
+++ Services/Inventory/Core/InventoryManagement.Core.Domain/UserAggregate/Entities/User.cs
@@ -3,13 +3,33 @@ using InventoryManagement.Core.Domain.ProductAggregate.Entities;
 
 namespace InventoryManagement.Core.Domain.UserAggregate.Entities;
 
-public class User : AggregateRoot
+public sealed class User : AggregateRoot
 {
+    public User()
+    {
+        AverageRating = 0;
+        ProductSold = 0;
+    }
+
+    public User(DateTime createdAt, string createdBy) : this()
+    {
+        CreatedAt = createdAt;
+        CreatedBy = createdBy;
+    }
+
+    public User(Guid id, string name, DateTime createdAt, string createdBy) : this(createdAt, createdBy)
+    {
+        Id = id;
+        Name = name;
+    }
+
     public string Name { get; set; } = null!;
 
     public double AverageRating { get; set; }
 
     public string? AvatarUrl { get; set; }
+
+    public string? CoverUrl { get; set; }
 
     public int ProductSold { get; set; }
 
